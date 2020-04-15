4d298e23b96606b80ad134db2482cf80867f45c5
shilparanic <shilparanic16@gmail.com>
2020-04-14 21:10:09 -0700

    this is first page

---
+++ src/gitprogram2/Fact1.java
@@ -0,0 +1,11 @@
+package gitprogram2;
+
+public class Fact1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("main1");
+System.out.println("pgm1");
+	}
+
+}
---
+++ src/gitprogram2/Fact3.java
@@ -0,0 +1,10 @@
+package gitprogram2;
+
+public class Fact3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("git fact3");
+	}
+
+}
